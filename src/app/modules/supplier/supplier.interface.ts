417e3f46cd3c341e1c814a22d5a50f8b27569b38
Abdullah Al Jubair <jubair2810@gmail.com>
2025-06-18 20:43:25 +0600

    added:interface

---
+++ src/app/modules/supplier/supplier.interface.ts
@@ -0,0 +1,10 @@
+export type TSupplier = {
+  name: string;
+  contact_person: string;
+  address: string;
+  phone: string;
+  fax: string;
+  email: string;
+  country: string;
+  city: string;
+};
