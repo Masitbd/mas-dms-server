48c01ffc8b69e36d163ae1fe1ae20981255218bc
Maruf-9270 <marufahmed9270@gmail.com>
2025-09-06 01:40:34 +0600

    Added purchase payment

--- src/app/modules/purchasePayments/purchasePayments.service.ts
+++ src/app/modules/purchasePayments/purchasePayments.service.ts
@@ -1,18 +1,47 @@
-import { IPurchasePayment } from './purchasePayments.interface';
-import { PurchasePayment } from './purchasePayments.model';
+import { Types } from "mongoose";
+import { IPurchasePayment } from "./purchasePayments.interface";
+import { PurchasePayment } from "./purchasePayments.model";
+import { Purchase } from "../purchases/purchases.model";
+import AppError from "../../errors/APiError";
+import { STATUS_CODES } from "http";
+import { HttpStatusCode } from "axios";
 
-const createPurchasePayment = async (payload: IPurchasePayment): Promise<IPurchasePayment> => {
+const createPurchasePayment = async (
+  payload: IPurchasePayment
+): Promise<IPurchasePayment> => {
+  const purchaseData = await Purchase.findOne({
+    _id: new Types.ObjectId(payload?.purchaseId),
+  });
+
+  if (!purchaseData) {
+    throw new AppError(HttpStatusCode.NotFound, "Purchase Not Found");
+  }
+  await Purchase.findOneAndUpdate(
+    { _id: new Types.ObjectId(payload.purchaseId) },
+    {
+      paidAmount: Number(purchaseData?.paidAmount) + payload.amount,
+      status:
+        purchaseData?.netPayable === purchaseData?.paidAmount + payload.amount
+          ? "paid"
+          : "due",
+    }
+  );
   const result = await PurchasePayment.create(payload);
+
   return result;
 };
 
 const getAllPurchasePayments = async (): Promise<IPurchasePayment[]> => {
-  const result = await PurchasePayment.find({}).populate('purchaseId');
+  const result = await PurchasePayment.find({}).populate("purchaseId");
   return result;
 };
 
-const getSinglePurchasePayment = async (id: string): Promise<IPurchasePayment | null> => {
-  const result = await PurchasePayment.findById(id).populate('purchaseId');
+const getSinglePurchasePayment = async (
+  id: string
+): Promise<IPurchasePayment[] | null> => {
+  const result = await PurchasePayment.find({
+    purchaseId: new Types.ObjectId(id),
+  });
   return result;
 };
 
@@ -26,7 +55,9 @@ const updatePurchasePayment = async (
   return result;
 };
 
-const deletePurchasePayment = async (id: string): Promise<IPurchasePayment | null> => {
+const deletePurchasePayment = async (
+  id: string
+): Promise<IPurchasePayment | null> => {
   const result = await PurchasePayment.findByIdAndDelete(id);
   return result;
 };
