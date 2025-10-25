e485712555b3e68642ad5aa8efa9ce59fd413b98
Maruf-9270 <marufahmed9270@gmail.com>
2025-10-25 09:55:04 +0600

    Fixed the medicine issue

--- src/app/modules/medicines/medicines.service.ts
+++ src/app/modules/medicines/medicines.service.ts
@@ -25,7 +25,17 @@ const createMedicine = async (payload: IMedicine): Promise<IMedicine> => {
 
 const getAllMedicinesFromDB = async (query: Record<string, any>) => {
   const medicineQuery = new QueryBuilder(
-    Medicine.find({ isDeleted: false }).select("medicineId name genericName"),
+    Medicine.find({ isDeleted: false })
+      .populate("category")
+      .select([
+        "medicineId",
+        "name",
+        "genericName",
+        "category",
+        "unit",
+        "openingBalanceRate",
+        "discount",
+      ]),
     query
   )
     .search(medicineSearchableFields)
